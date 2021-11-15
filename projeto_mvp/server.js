6350df9bb01d692d3ca96c3a4af439173b46fc86
Scheffel <aliners2020@gmail.com>
2021-11-15 17:01:55 -0300

    update

--- projeto_mvp/server.js
+++ projeto_mvp/server.js
@@ -3,3 +3,10 @@ const bodyParser= require("body-parser");
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+
+app.get('/',(req,res)=>{
+
+    res.json({
+      mensagem:'GRUPOU API'  
+    })
+})
